0e9b7aa6a30e2e3d53820768e1712f5f8d8e71f7
hfzgfr <hfzgfr@gmail.com>
2021-01-10 16:43:53 +0800

    userlist added

--- src/app.js
+++ src/app.js
@@ -4,27 +4,30 @@ import axios from 'axios';
 import store from './store/store';
 import { addUsers } from './actions/users';
 import { Provider } from 'react-redux';
-
 import Header from './components/Header';
+import UsersList from './components/UsersList';
+import './css/styles.css';
+
 class App extends React.Component {
     componentDidMount() {
-        axios.get('http://localhost:3000/users')
+        axios.get('/users')
             .then(response => {
                 console.log(response.data);
                 store.dispatch(addUsers(response.data.results));
             })
     }
     render() {
-return (
-    <div>
-        <Header />
-    </div>
-)
-}
+        return (
+            <div className="main-section">
+                <Header />
+                <UsersList />
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
 document.getElementById('root'));
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1>Random Users Application</h1>
+      <h1 className="header">Random Users Application</h1>;
     </div>
   )
 }
